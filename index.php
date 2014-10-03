5257844c678152e08d166fa41a9134e015261b8e
jeissonp <jeissonp@gmail.com>
2014-10-03 10:24:21 -0500

    update 7.31

--- index.php
+++ index.php
@@ -14,9 +14,6 @@
 /**
  * Root directory of Drupal installation.
  */
-ini_set('memory_limit', '256M');
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
 define('DRUPAL_ROOT', getcwd());
 
 require_once DRUPAL_ROOT . '/includes/bootstrap.inc';
